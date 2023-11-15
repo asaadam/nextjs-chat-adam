d10b653c53c0a11bd693911d737fdc3116f780b0
asaadam <graha.asa@gmail.com>
2023-11-15 10:33:48 +0700

    fix: header

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,23 @@
 import { Button } from '@/components/ui/button'
 import {
   IconInstagramOutline,
+  IconMenu,
   IconMoon,
   IconTwitterOutline
 } from '@/components/ui/icons'
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarTrigger
+} from '@/components/ui/menubar'
 import Image from 'next/image'
 
 export async function Header() {
   return (
-    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
+    <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl">
       <div className="flex items-center">
         <Image
           src="/blackbox-logo.png"
@@ -17,7 +26,7 @@ export async function Header() {
           alt="blackbox-logo"
         />
       </div>
-      <div className="flex items-center justify-end space-x-2">
+      <div className="hidden items-center justify-end space-x-2 lg:flex">
         <Button variant="outline">
           <Image
             src="/icon-telegram.svg"
@@ -47,6 +56,43 @@ export async function Header() {
         <Button variant="outline" size="icon" className="border-none">
           <IconMoon className="h-3.5 w-3.5" />
         </Button>
+      </div>
+      <div className="flex items-center justify-end space-x-2 lg:hidden">
+        <Menubar className="border-none">
+          <MenubarMenu>
+            <MenubarTrigger>
+              <IconMenu />
+            </MenubarTrigger>
+            <MenubarContent className="mr-2">
+              <MenubarItem className="py-3">
+                <Image
+                  src="/icon-telegram.svg"
+                  width={16}
+                  height={16}
+                  alt="vscode-icon"
+                  className="mr-3"
+                />{' '}
+                Telegram
+              </MenubarItem>
+              <MenubarItem className="py-3">
+                <Image
+                  src="/icon-vscode-white.svg"
+                  width={16}
+                  height={16}
+                  alt="vscode-icon"
+                  className="mr-3"
+                />{' '}
+                Install VSCode Extension
+              </MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>
+                <IconTwitterOutline className="mr-2 h-4 w-4" />
+                <IconInstagramOutline className="mr-2 h-4 w-4" />
+                <IconMoon className="h-4 w-4" />
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
       </div>
     </header>
   )
--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -637,6 +637,40 @@ function IconInstagramOutline({
   )
 }
 
+function IconMenu({ className, ...props }: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      className={cn('h-6 w-6', className)}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M3 12H21"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M3 6H21"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M3 18H21"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  )
+}
+
 export {
   IconEdit,
   IconNextChat,
@@ -669,5 +703,6 @@ export {
   IconGallery,
   IconArrowRightLong,
   IconTwitterOutline,
-  IconInstagramOutline
+  IconInstagramOutline,
+  IconMenu
 }
